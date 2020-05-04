21031262247aaee90833d148f8425c31272a5522
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2020-05-04 17:18:33 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+class App extends Component {
 
-  const increment = () => {
-    setCount(count + 1);
+  state = {
+    count: 0
   };
 
-  return (
-    <div>
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times `
+  }
+  componentDidUpdate() {
+    document.title = `Clicked ${this.state.count} times `
+  }
+  render() {
+
+    return (
+      <div>
       <h2>counter app</h2>
-      <button onClick={increment}>Clicked {count} times</button>
-    </div>
-  );
-};
+      <button onClick={this.increment}>Clicked {this.state.count} times</button>
+      </div>
+      );
+    }
+}
 
 export default App;
-
-// class App extends Component {
-
-//   state = {
-//     count: 0
-//   };
-
-//   increment = () => {
-//     this.setState({
-//       count: this.state.count + 1
-//     })
-//   }
-//   render() {
-
-//     return (
-//       <div>
-//       <h2>counter app</h2>
-//       <button onClick={this.increment}>Clicked {this.state.count} times</button>
-//       </div>
-//       );
-//     }
-// }
-
-// export default App;
